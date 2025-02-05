afebf6f85a31f5836f6f27398c1ecb20a995d11b
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2025-02-05 11:04:35 -0500

    Create Linear.cpp

---
+++ Linear.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+class matrixInfo{
+    public:
+        const int row,col;
+    private:
+        double* value;
+};
+
+void makeMatrix(matrixInfo m){
+    const int a=m.row,b=m.col;
+    double matrix[a][b];
+}
+
+int main() {
+    
+}
