c6a071548ba4b7c234d956040943ca7967735a7c
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2025-02-07 12:51:26 -0500

    Update Linear.cpp

--- Linear.cpp
+++ Linear.cpp
@@ -5,19 +5,31 @@
 using namespace std;
 
 
-
-class matrixInfo{
-    public:
-        const int row,col;
-    private:
+struct matrixInfo{
+        int row,col;
         double* value;
+
 };
 
-void makeMatrix(matrixInfo m){
+vector<vector<double>> makeMatrix(matrixInfo m){
     const int a=m.row,b=m.col;
-    double matrix[a][b];
+    vector<vector<double>> matrix(a,vector<double>(b, 0));
+    return matrix;
+}
+
+matrixInfo makeMatrixInfo(int rows, int cols){
+    matrixInfo m;
+    m.row=rows;
+    m.col=cols;
+    return m;
 }
 
 int main() {
-    
+    matrixInfo m = makeMatrixInfo(3,4);
+    vector<vector<double>> matrix = makeMatrix(m);
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;i<matrix[i].size();j++){
+            cout << matrix[i][j];
+        }
+    }
 }
