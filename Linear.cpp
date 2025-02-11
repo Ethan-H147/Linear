4fc42a05601acbd1d53c36a14f076be8d64febed
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2025-02-10 19:17:12 -0500

    changed one letter

--- Linear.cpp
+++ Linear.cpp
@@ -28,7 +28,7 @@ int main() {
     matrixInfo m = makeMatrixInfo(3,4);
     vector<vector<double>> matrix = makeMatrix(m);
     for(int i=0;i<matrix.size();i++){
-        for(int j=0;i<matrix[i].size();j++){
+        for(int j=0;j<matrix[i].size();j++){
             cout << matrix[i][j];
         }
     }
