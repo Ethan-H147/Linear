f2b38cc94dbf42c27b7b0e42bb4f120a1894a9e9
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2025-02-11 11:00:06 -0500

    Update Linear.cpp

--- Linear.cpp
+++ Linear.cpp
@@ -24,12 +24,40 @@ matrixInfo makeMatrixInfo(int rows, int cols){
     return m;
 }
 
+void solveMatrix(vector<vector<double>> matrix){
+    double tempfactor = matrix[0][0]/matrix[1][0];
+    vector<double> temp = {matrix[1][0]*tempfactor,matrix[1][1]*tempfactor,matrix[1][2]*tempfactor};
+    
+    vector<double> xcancelled1 = {matrix[0][0]-temp[0],matrix[0][1]-temp[1],matrix[0][2]-temp[2]};
+    for(int j=0;j<3;j++){
+        cout << xcancelled[j] << " ";
+    }
+    cout << endl;
+    tempfactor = matrix[0][0]/matrix[2][0];
+    temp = {matrix[2][0]*tempfactor,matrix[2][1]*tempfactor,matrix[2][2]*tempfactor};
+    for(int j=0;j<3;j++){
+        cout << temp[j] << " ";
+    }
+    cout << endl;
+    vector<double> ycancelled = {xcancelled[0]-temp[0],xcancelled[1]-temp[1],xcancelled[2]-temp[2]};
+    for(int j=0;j<3;j++){
+        cout << ycancelled[j] << " ";
+    }
+
+}
+
 int main() {
-    matrixInfo m = makeMatrixInfo(3,4);
-    vector<vector<double>> matrix = makeMatrix(m);
+    matrixInfo m = makeMatrixInfo(3,3);
+    vector<vector<double>> matrix = /*makeMatrix(m)*/{{3.0,4.0,5.0}, 
+    {2.0,3.0,4.0},
+    {1,2,3}};
+    //matrix = 
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[i].size();j++){
-            cout << matrix[i][j];
+            //cout << matrix[i][j] << " ";
         }
+        cout << "\n";
     }
+    solveMatrix(matrix);
+
 }
