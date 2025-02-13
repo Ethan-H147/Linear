ecdc38b38cf75fd842690dfd07ca874ddc7e831e
Ethan-H147 <97569719+Ethan-H147@users.noreply.github.com>
2025-02-13 11:03:04 -0500

    Update Linear.cpp

--- Linear.cpp
+++ Linear.cpp
@@ -24,26 +24,40 @@ matrixInfo makeMatrixInfo(int rows, int cols){
     return m;
 }
 
+int determinant(vector<vector<double>> matrix){
+    if(matrix.size()==2&&matrix[0].size()==2){
+        return matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
+    }
+    else{
+        int det=0;
+        for(int i=0;i<matrix.size();i++){
+            
+        }
+    }
+}
+
+vector<vector<double>> inverseMatrix(vector<vector<double>> matrix){
+
+}
+
 void solveMatrix(vector<vector<double>> matrix){
     double tempfactor = matrix[0][0]/matrix[1][0];
     vector<double> temp = {matrix[1][0]*tempfactor,matrix[1][1]*tempfactor,matrix[1][2]*tempfactor};
     
     vector<double> xcancelled1 = {matrix[0][0]-temp[0],matrix[0][1]-temp[1],matrix[0][2]-temp[2]};
     for(int j=0;j<3;j++){
-        cout << xcancelled[j] << " ";
+        cout << xcancelled1[j] << " ";
     }
     cout << endl;
     tempfactor = matrix[0][0]/matrix[2][0];
     temp = {matrix[2][0]*tempfactor,matrix[2][1]*tempfactor,matrix[2][2]*tempfactor};
-    for(int j=0;j<3;j++){
-        cout << temp[j] << " ";
-    }
     cout << endl;
-    vector<double> ycancelled = {xcancelled[0]-temp[0],xcancelled[1]-temp[1],xcancelled[2]-temp[2]};
+    vector<double> xcancelled2 = {matrix[0][0]-temp[0],matrix[0][1]-temp[1],matrix[0][2]-temp[2]};
     for(int j=0;j<3;j++){
-        cout << ycancelled[j] << " ";
+        cout << xcancelled2[j] << " ";
     }
-
+    tempfactor=xcancelled1[1]/xcancelled2[1];
+    
 }
 
 int main() {
@@ -54,7 +68,7 @@ int main() {
     //matrix = 
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[i].size();j++){
-            //cout << matrix[i][j] << " ";
+            cout << matrix[i][j] << " ";
         }
         cout << "\n";
     }
